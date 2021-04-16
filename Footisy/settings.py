4d63f6d90f3a78baa561e511dd026a9ce2ec0b8c
jmiranda-conncoll <70863053+jmiranda-conncoll@users.noreply.github.com>
2021-04-16 12:37:08 -0400

    push ready

--- Footisy/settings.py
+++ Footisy/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 LOGIN_URL = '/signUps/user_login/'
 
